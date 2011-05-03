d38ac74150faeb684aa552c41e32ea14a9dee20e
Patrick Gasnier <patrick734734@gmail.com>
2011-05-02 23:39:56 -0300

    Versão Protótipo funcional

--- Pomodoro-0/Pomodoro-0/Pomodoro_0ViewController.h
+++ Pomodoro-0/Pomodoro-0/Pomodoro_0ViewController.h
@@ -8,8 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface Pomodoro_0ViewController : UIViewController {
+@interface Pomodoro_0ViewController : UIViewController 
+{
+    IBOutlet UILabel *textLabel, *sucesso;
+    int minutos, segundos;
     
+    BOOL running;
+    
+    NSTimer *time;
+    
+    IBOutlet UIButton *up, *down;
 }
+
+@property (nonatomic, retain)UILabel *textLabel, *sucesso;
+@property (nonatomic)int segundos, minutos;
+@property (nonatomic, retain) NSTimer *time;
+@property (nonatomic, retain) UIButton *up, *down;
+@property (nonatomic) BOOL running;
+
+- (IBAction)startTimer:(id)sender;
+- (IBAction)stopTimer:(id)sender;
+- (void) resetTimer;
+- (void) countSecond;
+- (void) countMinute;
+- (void)enableButtons;
+- (void)disableButtons;
 
 @end
--- Pomodoro-0/Pomodoro-0/Pomodoro_0ViewController.m
+++ Pomodoro-0/Pomodoro-0/Pomodoro_0ViewController.m
@@ -10,8 +10,18 @@
 
 @implementation Pomodoro_0ViewController
 
+@synthesize textLabel, sucesso;
+@synthesize segundos, minutos;
+@synthesize time;
+@synthesize up, down;
+@synthesize running;
+
 - (void)dealloc
 {
+    [textLabel release];
+    [sucesso release];
+    [up release];
+    [down release];
     [super dealloc];
 }
 
@@ -25,13 +35,20 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    segundos = 10;
+    minutos = 0;
+    running = NO;
+    NSString *texto = [[NSString alloc] initWithFormat:@"%2d : %2d", self.minutos, self.segundos];
+    
+    [textLabel setText:texto];
+    [texto release];
+    
 }
-*/
 
 - (void)viewDidUnload
 {
@@ -44,6 +61,146 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Interface Functions
+- (IBAction)startTimer:(id)sender
+{
+    if(!running)
+    {
+        if(![sucesso isHidden])
+        {
+            [sucesso setHidden:YES];
+        }
+        
+        [self disableButtons];
+        
+        time = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                target:self
+                                               selector:@selector(countSecond)
+                                               userInfo:nil 
+                                                repeats:YES];
+        running = YES;
+    }
+}
+
+- (void)enableButtons
+{
+    [up setEnabled:YES];
+    [down setEnabled:YES];    
+}
+
+- (void)disableButtons
+{
+    [up setEnabled:NO];
+    [down setEnabled:NO];  
+}
+
+- (IBAction)stopTimer:(id)sender
+{
+    if(running)
+    {
+        [time invalidate];
+        
+        [self enableButtons];
+        running = NO;
+    }
+    [self resetTimer];
+    [sucesso setHidden:YES];
+}
+
+- (void) resetTimer
+{
+    segundos = 0;
+    minutos = 25;
+    
+    NSString *texto = [[NSString alloc] initWithFormat:@"%2d : %2d", self.minutos, self.segundos];
+    
+    [textLabel setText:texto];
+    [texto release];
+}
+
+- (void) countSecond
+{
+    if(minutos == 0)
+    {
+        if(segundos != 0)
+        {
+            segundos--;
+        }
+        else
+        {
+            [time invalidate];
+            NSString *texto = [[NSString alloc] initWithFormat:@"Sucesso!"];
+            [sucesso setText:texto];
+            [sucesso setHidden:NO];
+            [texto release];
+            [self resetTimer];
+            running = NO;
+        }
+    }
+    else
+    {
+        if(segundos == 0)
+        {
+            segundos = 59;
+            [self countMinute];
+        }
+        else
+        {
+            segundos--;
+        }
+    }
+    
+    NSString *texto = [[NSString alloc] initWithFormat:@"%2d : %2d", self.minutos, self.segundos];
+    
+    [textLabel setText:texto];
+    [texto release];
+}
+
+- (void) countMinute
+{
+    minutos--;
+}
+
+- (IBAction) oneMore:(id)sender
+{
+    if(minutos < 59)
+    {
+        minutos++;
+        
+        NSString *texto = [[NSString alloc] initWithFormat:@"%2d : %2d", self.minutos, self.segundos];
+        
+        [textLabel setText:texto];
+        [texto release];
+    }
+    else
+    {
+        NSString *texto = [[NSString alloc] initWithFormat:@"N‹o pode!"];
+        [sucesso setText:texto];
+        [sucesso setHidden:NO];
+        [texto release];
+    }
+}
+
+- (IBAction) OneLess:(id)sender
+{
+    if(minutos > 0)
+    {
+        minutos--;
+        
+        NSString *texto = [[NSString alloc] initWithFormat:@"%2d : %2d", self.minutos, self.segundos];
+        
+        [textLabel setText:texto];
+        [texto release];
+    }
+    else
+    {
+        NSString *texto = [[NSString alloc] initWithFormat:@"N‹o pode!"];
+        [sucesso setText:texto];
+        [sucesso setHidden:NO];
+        [texto release];
+    }
 }
 
 @end
